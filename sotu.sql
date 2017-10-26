409e96c854d94063c6f061e0f968fabe0f0f49e9
jack <guangjian.cgj@alibaba-inc.com>
2017-10-27 02:31:07 +0800

    SELECT   @mb := round((sum(DATA_LENGTH) + sum(INDEX_LENGTH)) / (1024 * 1024), 2),   concat(@mb, 'MB') FROM information_schema.tables WHERE table_schema = 'sotu';

--- sotu.sql
+++ sotu.sql
@@ -1,23 +1,39 @@
-CREATE SCHEMA `sotu` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA `sotu`
+  DEFAULT CHARACTER SET utf8;
 
 ALTER TABLE `sotu`.`image`
   ADD INDEX `idx_category` (`category` ASC),
   ADD UNIQUE INDEX `uk_url` (`url` ASC);
 
-show TABLES ;
-desc image;
-desc search_key_word;
+SHOW TABLES;
+DESC image;
+DESC search_key_word;
 
-SELECT count(*) from image;
-SELECT * FROM image;
-SELECT * FROM search_key_word where key_word = '';
-SELECT count(*) FROM search_key_word;
-delete from search_key_word WHERE key_word = '';
+SELECT count(*)
+FROM image;
+SELECT *
+FROM image;
+SELECT *
+FROM search_key_word
+WHERE key_word = '';
+SELECT count(*)
+FROM search_key_word;
+DELETE FROM search_key_word
+WHERE key_word = '';
 
-SELECT * FROM search_key_word;
+SELECT *
+FROM search_key_word;
 
-SELECT * FROM image WHERE source_type=1;
+SELECT *
+FROM image
+WHERE source_type = 1;
 
+
+SELECT
+  @mb := round((sum(DATA_LENGTH) + sum(INDEX_LENGTH)) / (1024 * 1024), 2),
+  concat(@mb, 'MB')
+FROM information_schema.tables
+WHERE table_schema = 'sotu';
 
 
 
